3335a13849b2d8b4e51f56477fd885aab9e0e83f
Ashleigh Petty <petty.ashleigh@yahoo.com>
2020-05-12 04:41:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken? (board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
-    returns FALSE
-  else returns TRUE
+    return FALSE
+  else return TRUE
 end
 end
